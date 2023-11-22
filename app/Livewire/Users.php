e049d14214e4e445732e3c7bdfce88b83da31d18
sadeghbiglar <sbmail555@gmail.com>
2023-11-22 23:21:02 +0330

    create user form in users

--- app/Livewire/Users.php
+++ app/Livewire/Users.php
@@ -3,18 +3,39 @@
 namespace App\Livewire;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
 use Livewire\WithPagination;
 
 class Users extends Component
 {
     use WithPagination;
-    public $role_id = '';
+    #[Rule('required|min:2|max:50')]
+    public $name='';
+    #[Rule('required|email|unique:users')]
+    public $email='';
+    #[Rule('required|min:5')]
+    public $password='';
+    #[Rule('required')]
+    public $role_id='';
     public $search = '';
+    public  function createNewUser(){
+        $validated=$this->validate();
+        User::create(
 
+            [
+                'name'=>$validated['name'],
+                'email'=>$validated['email'],
+                'password'=>Hash::make( $validated['password']),
+                'role_id'=>$validated['role_id']
+            ]
+        );
+        $this->reset(['name','email','password','role_id']);
+        request()->session()->flash('success','user created success');
+    }
     public function render()
     {
-       // $users=User::latest()->where('name','like',"%{{$this->search}}%")->paginate(3);
         return view('livewire.users',[
             'users'=>User::latest()->where('name','like',"%{$this->search}%")->paginate(3)]);
     }
--- resources/views/livewire/users.blade.php
+++ resources/views/livewire/users.blade.php
@@ -42,5 +42,68 @@
 
     </div>
 
+    <!-- component -->
 
+    <div class="bg-grey-lighter min-h-screen flex flex-col">
+        <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+
+            <div class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+                <h1 class="mb-8 text-3xl text-center">افزودن کاربر جدید</h1>
+                <form wire:submit="createNewUser" action="">
+                    <input
+                        wire:model="name"
+                        type="text"
+                        class="block border border-grey-light w-full p-3 rounded mb-4"
+                        name="username"
+                        placeholder="User Name" />
+                    @error('name')
+                    <span class="text-red-500 text-xs">{{$message}}</span>
+                    @enderror
+                    <input
+                        wire:model="email"
+                        type="text"
+                        class="block border border-grey-light w-full p-3 rounded mb-4"
+                        name="email"
+                        placeholder="Email" />
+                    @error('email')
+                    <span class="text-red-500 text-xs">{{$message}}</span>
+                    @enderror
+                    <input
+                        wire:model="password"
+                        type="password"
+                        class="block border border-grey-light w-full p-3 rounded mb-4"
+                        name="password"
+                        placeholder="Password" />
+                    @error('password')
+                    <span class="text-red-500 text-xs">{{$message}}</span>
+                    @enderror
+                    <input
+                        wire:model="role_id"
+                        type="text"
+                        class="block border border-grey-light w-full p-3 rounded mb-4"
+                        name="role_id"
+                        placeholder="role_id" />
+                    @error('role_id')
+                    <span class="text-red-500 text-xs">{{$message}}</span>
+                    @enderror
+                    <button
+                        wire:click.prevent="createNewUser"
+                        type="submit"
+                        class="w-full text-center py-3 rounded bg-green-500 text-white hover:bg-green-500 focus:outline-none my-1"
+                    >ایجاد کاربر</button>
+
+                </form>
+                @if(session('success'))
+                    <span>{{session('success')}}</span>
+                @endif
+
+            </div>
+
+
+        </div>
+
+    </div>
 </div>
+
+
+
